bd1c8525f2042a3d857277f3bf2cd6fedea16e66
NotJackIsMercury <72420946+NotJackIsMercury@users.noreply.github.com>
2021-05-19 21:25:07 +0800

    Delete java directory

--- java/com/github/notjackismercury/xyslabs/state/property/XyProperties.java
+++
@@ -1,8 +0,0 @@
-package com.github.notjackismercury.xyslabs.state.property;
-
-import net.minecraft.block.enums.SlabType;
-import net.minecraft.state.property.EnumProperty;
-
-public class XyProperties {
-	public static final EnumProperty<SlabType> SLAB_HALF = EnumProperty.of("half", SlabType.class);
-}
